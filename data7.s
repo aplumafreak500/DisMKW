3db6890dee3047db28a12734b3c45b839d48d27b
aplumafreak500 <aplumafreak500@gmail.com>
2018-06-16 18:40:51 -0400

    Dump and document a bit more of D4

--- data7.s
+++ data7.s
@@ -3,7 +3,14 @@
 	.set Data7Size, Data7End - Data7Start
 	.global Data7Start
 Data7Start:
-	.incbin "basemain.dol", 0x29d1a0, 0x21a0
+	.incbin "basemain.dol", 0x29d1a0, 0x60
+	.global gUnknown_60
+gUnknown_60: # 0x80382c80
+	.4byte 0
+	.global gUnknown_64
+gUnknown_64: # 0x80382c84
+	.4byte 0
+	.incbin "basemain.dol", 0x29d208, 0x2138
 	.global Data7End
 Data7End:
 	.global DolEnd
