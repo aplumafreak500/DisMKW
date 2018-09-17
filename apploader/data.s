e41c359097ef183a51c442d445756c10e98e4444
aplumafreak500 <aplumafreak500@gmail.com>
2018-09-17 17:13:26 -0400

    Link text and data of apploader and explicity set apploader entry point
    Remove a small mistake causing apploader.img to be > 2 GiB in size #ButterflyEffect

--- apploader/data.s
+++ apploader/data.s
@@ -1,32 +1,89 @@
 	.data
 	
+	.global FailedAssertStr
+FailedAssertStr:
 	.asciz "Failed assertion len <= RETURNFUNC_MAXSIZE\0"
-	.asciz "\nApploader Initialized.\n\0\0\0"
+	.global ApploaderInitializedStr
+ApploaderInitializedStr:
+	.asciz "\nApploader Initialized.\n\0"
+	.2byte 0
+	.global ApploaderBuiltStr
+ApploaderBuiltStr:
 	.asciz "This Apploader built %s %s for RVL\n"
+	.global BuildDate
+BuildDate:
 	.asciz "Dec  6 2007"
-	.asciz "12:45:59\0\0\0"
+	.global BuildTime
+BuildTime:
+	.asciz "12:45:59\0"
+	.2byte 0
 	.4byte 0
-	.asciz "\nAPPLOADER WARNING >>> Older version of DEVKIT BOOT PROGRAM.\nAPPLOADER WARNING >>> Use v1.07 or later.\n"
+	.global OldVersionStr
+OldVersionStr:
+	.ascii "\nAPPLOADER WARNING >>> Older version of DEVKIT BOOT PROGRAM.\n"
+	.global OldVersionStr
+OldVersionStr2:
+	.asciz "APPLOADER WARNING >>> Use v1.07 or later.\n"
+	.global FSTLengthStr
+FSTLengthStr:
 	.asciz "APPLOADER ERROR >>> FSTLength(%d) in BB2 is greater than FSTMaxLength(%d)\n\0"
 	.4byte 0
+	.global DebugMonitorSizeStr
+DebugMonitorSizeStr:
 	.asciz "APPLOADER ERROR >>> Debug monitor size (%d) should be a multiple of 32\n"
+	.global SimMemSizeStr
+SimMemSizeStr:
 	.asciz "APPLOADER ERROR >>> Simulated memory size (%d) should be a multiple of 32\n\0"
-	.asciz "APPLOADER ERROR >>> [Physical memory size(0x%x)] - [Console simulated memory size(0x%x)]\nAPPLOADER ERROR >>> must be greater than debug monitor size(0x%x)\n"
-	.asciz "APPLOADER WARNING >>> Physical memory size is 0x%x bytes.\nAPPLOADER WARNING >>> Console simulated memory size (0x%x) must be smaller than or equal to the Physical memory size\nAPPLOADER WARNING >>> Use physical memory size as the console simulated memory size.....\n\0\0\0"
-	.asciz "APPLOADER ERROR >>> Illegal FST destination address! (0x%x)\n\0\0\0"
-	.asciz "APPLOADER ERROR >>> Total size of text/data sections of the dol file are too big (%d(0x%08x) bytes). Currently the limit is set as %d(0x%08x) bytes\n\0\0\0"
+	.global MemSizeStr
+MemSizeStr:
+	.ascii "APPLOADER ERROR >>> [Physical memory size(0x%x)] - [Console simulated memory size(0x%x)]\n"
+	.asciz "APPLOADER ERROR >>> must be greater than debug monitor size(0x%x)\n"
+	.global PhysMemSizeStr
+PhysMemSizeStr:
+	.ascii "APPLOADER WARNING >>> Physical memory size is 0x%x bytes.\n"
+	.global ConsoleMemSizeStr
+ConsoleMemSizeStr:
+	.ascii "APPLOADER WARNING >>> Console simulated memory size (0x%x) must be smaller than or equal to the Physical memory size\n"
+	.global UsePhysMemSizeStr
+UsePhysMemSizeStr:
+	.asciz "APPLOADER WARNING >>> Use physical memory size as the console simulated memory size.....\n\0"
+	.2byte 0
+	.global IllegalFSTDestAddrStr
+IllegalFSTDestAddrStr:
+	.asciz "APPLOADER ERROR >>> Illegal FST destination address! (0x%x)\n\0"
+	.2byte 0
+	.global SectionSizeTooBigStr
+SectionSizeTooBigStr:
+	.asciz "APPLOADER ERROR >>> Total size of text/data sections of the dol file are too big (%d(0x%08x) bytes). Currently the limit is set as %d(0x%08x) bytes\n\0"
+	.2byte 0
+	.global DolSectionExceedsStr
+DolSectionExceedsStr:
 	.asciz "APPLOADER ERROR >>> One of the sections in the dol file exceeded its boundary. All the sections should not exceed 0x%08x (production mode)\n"
+	.global DolSectionExceedsStr2
+DolSectionExceedsStr2:
 	.asciz "\n\nAPPLOADER WARNING >>> One of the sections in the dol file exceeded its boundary. All the sections should not exceed 0x%08x in production mode.\n\n\0"
+	.global DolSectionExceedsStr3
+DolSectionExceedsStr3:
 	.asciz "APPLOADER ERROR >>> One of the sections in the dol file exceeded its boundary. All the sections should not exceed 0x%08x (development mode).\n"
 	.2byte 0
-	.asciz "APPLOADER ERROR >>> Too big text segment! (0x%x - 0x%x)\n\0\0\0"
-	.asciz "APPLOADER ERROR >>> Too big data segment! (0x%x - 0x%x)\n\0\0\0"
+	.global TooBigTextSectionStr
+TooBigTextSectionStr:
+	.asciz "APPLOADER ERROR >>> Too big text segment! (0x%x - 0x%x)\n\0"
+	.2byte 0
+	.global TooBigDataSectionStr
+TooBigDataSectionStr:
+	.asciz "APPLOADER ERROR >>> Too big data segment! (0x%x - 0x%x)\n\0"
+	.2byte 0
+	.global FailedAssertStr2
+FailedAssertStr2:
 	.asciz "Failed assertion SecondTimeForThePart == TRUE"
 	.2byte 0
-	.4byte 0x81200658, 0x81200658, 0x81200698, 0x81200704
-	.4byte 0x812008A0, 0x81200A78, 0x81200AA8, 0x81200D00
-	.4byte 0x81200E28, 0x81200F50, 0x81200FE4, 0x81201054
-	.4byte 0x812010B8
+	.global gUnknown_
+gUnknown_:
+	.4byte apl_658, apl_658, apl_698, apl_704
+	.4byte apl_8a0, apl_a78, apl_aa8, apl_d00
+	.4byte apl_e28, apl_f50, apl_fe4, apl_1054
+	.4byte apl_10b8
 	
 	.space 412 # Black and gold, yo.
 
--- apploader/text.s
+++ apploader/text.s
@@ -1,2 +1,58 @@
 	.text
-	.incbin "baseapploader.img", 0x20, 0x12c0
+	
+	.global apl_0
+apl_0:
+	.incbin "baseapploader.img", 0x20, 0x294
+	
+	.global apl_entrypoint
+apl_entrypoint:
+	.incbin "baseapploader.img", 0x2b4, 0x3c4
+	
+	.global apl_658
+apl_658:
+	.incbin "baseapploader.img", 0x678, 0x40
+	
+	.global apl_698
+apl_698:
+	.incbin "baseapploader.img", 0x6b8, 0x6c
+	
+	.global apl_704
+apl_704:
+	.incbin "baseapploader.img", 0x724, 0x19c
+	
+	.global apl_8a0
+apl_8a0:
+	.incbin "baseapploader.img", 0x8c0, 0x1d8
+	
+	.global apl_a78
+apl_a78:
+	.incbin "baseapploader.img", 0xa98, 0x30
+	
+	.global apl_aa8
+apl_aa8:
+	.incbin "baseapploader.img", 0xac8, 0x258
+	
+	.global apl_d00
+apl_d00:
+	.incbin "baseapploader.img", 0xd20, 0x128
+	
+	.global apl_e28
+apl_e28:
+	.incbin "baseapploader.img", 0xe48, 0x128
+	
+	.global apl_f50
+apl_f50:
+	.incbin "baseapploader.img", 0xf70, 0x94
+	
+	.global apl_fe4
+apl_fe4:
+	.incbin "baseapploader.img", 0x1004, 0x70
+	
+	.global apl_1054
+apl_1054:
+	.incbin "baseapploader.img", 0x1074, 0x64
+	
+	.global apl_10b8
+apl_10b8:
+	.incbin "baseapploader.img", 0x10d8, 0x208
+	
