904a42e302becd2da0e633cbcac2e4fff08e181a
aplumafreak500 <aplumafreak500@gmail.com>
2018-10-25 16:25:26 -0400

    .extabindex - .extab linking

--- apploader/loader/extab.s
+++ apploader/loader/extab.s
@@ -1,11 +1,62 @@
 	.data
-	.4byte 0x18080000, 0, 0x18080000, 0
-	.4byte 0x10080000, 0, 0x18480000, 0
-	.4byte 0x10080000, 0, 0x30080000, 0
-	.4byte 0x8080000, 0, 0x80000, 0
-	.4byte 0x18080000, 0, 0x50080000, 0
-	.4byte 0x204a0000, 0, 0x20480000, 0
-	.4byte 0x80080000, 0, 0x10080000, 0
-	.4byte 0x18080000, 0, 0x28080000, 0
-	.4byte 0x28080000, 0, 0x80080000, 0
+	.global extab_entry0
+extab_entry0:
+	.4byte 0x18080000, 0
+	.global extab_entry1
+extab_entry1:
+	.4byte 0x18080000, 0
+	.global extab_entry2
+extab_entry2:
+	.4byte 0x10080000, 0
+	.global extab_entry3
+extab_entry3:
+	.4byte 0x18480000, 0
+	.global extab_entry4
+extab_entry4:
+	.4byte 0x10080000, 0
+	.global extab_entry5
+extab_entry5:
+	.4byte 0x30080000, 0
+	.global extab_entry6
+extab_entry6:
 	.4byte 0x8080000, 0
+	.global extab_entry7
+extab_entry7:
+	.4byte 0x80000, 0
+	.global extab_entry8
+extab_entry8:
+	.4byte 0x80000, 0
+	.global extab_entry9
+extab_entry9:
+	.4byte 0x18080000, 0
+	.global extab_entry10
+extab_entry10:
+	.4byte 0x50080000, 0
+	.global extab_entry11
+extab_entry11:
+	.4byte 0x204a0000, 0
+	.global extab_entry12
+extab_entry12:
+	.4byte 0x20480000, 0
+	.global extab_entry13
+extab_entry13:
+	.4byte 0x80080000, 0
+	.global extab_entry14
+extab_entry14:
+	.4byte 0x10080000, 0
+	.global extab_entry15
+extab_entry15:
+	.4byte 0x18080000, 0
+	.global extab_entry16
+extab_entry16:
+	.4byte 0x28080000, 0
+	.global extab_entry17
+extab_entry17:
+	.4byte 0x28080000, 0
+	.global extab_entry18
+extab_entry18:
+	.4byte 0x80080000, 0
+	.global extab_entry19
+extab_entry19:
+	.4byte 0x8080000, 0
+
